89971c927d0af25fd28d9a50a037d5b8f5bec2af
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-12-19 14:44:05 +0200

    Increment / reset

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{counter}</Text>
+      <Text>Increment counter</Text>
+      <Button title="Increment" onPress={() => setCounter(counter+1)} />
+      <Button title="Reset" onPress={() => setCounter(0)} />
     </View>
   );
 }
